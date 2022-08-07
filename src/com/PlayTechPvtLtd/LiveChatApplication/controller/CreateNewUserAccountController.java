4244b5c0d8d366451130eacf25efa42227889266
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-08-07 19:47:25 +0530

    Add Login form and Create New User Account fxml,styleSheet,controller class

---
+++ src/com/PlayTechPvtLtd/LiveChatApplication/controller/CreateNewUserAccountController.java
@@ -0,0 +1,49 @@
+package com.PlayTechPvtLtd.LiveChatApplication.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+public class CreateNewUserAccountController {
+    public TextField txtUserName;
+    public PasswordField txtPassword;
+    public TextField txtEmail;
+    public TextField txtPhoneNumber;
+    public RadioButton male;
+    public RadioButton female;
+    public ToggleGroup Gender;
+    public Label checkEmail;
+    public Label success;
+    public Label goBack;
+    public Button btnLogin;
+    public ImageView btnBack;
+    public Label nameExists;
+    public Label controlRegLabel;
+    public Label checkEmail1;
+    public Label nameExists1;
+    public TextField userName;
+    public PasswordField passWord;
+    public Button btnSignUp;
+    public Label loginNotifier;
+    public AnchorPane loginPane;
+    public AnchorPane signUpPane;
+
+
+    public void CreateAccountOnAction(ActionEvent actionEvent) {
+    }
+
+    public void signUpInOnAction(ActionEvent actionEvent) {
+    }
+
+    public void backButtonOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
+}
