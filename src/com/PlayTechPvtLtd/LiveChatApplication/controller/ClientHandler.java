4b597a27d89a4e831dc8c9ae005d5e1c6dd079f1
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-08-07 17:16:30 +0530

    Update ClientHandler Class

--- src/com/PlayTechPvtLtd/LiveChatApplication/controller/ClientHandler.java
+++ src/com/PlayTechPvtLtd/LiveChatApplication/controller/ClientHandler.java
@@ -1,11 +1,53 @@
 package com.PlayTechPvtLtd.LiveChatApplication.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 
 public class ClientHandler extends Thread {
 
+    String message = "";
+    private ArrayList<ClientHandler> clients;
+    private Socket socket;
+    private BufferedReader reader;
+    private PrintWriter writer;
 
     public ClientHandler(Socket socket, ArrayList<ClientHandler> clients) {
+        try {
+            this.socket = socket;
+            this.clients = clients;
+            this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.writer = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            while ((message = reader.readLine()) != null) {
+                if (message.equalsIgnoreCase("exit")) {
+                    break;
+                }
+                for (ClientHandler cl : clients) {
+                    cl.writer.println(message);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+                writer.close();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
