5c16076070b0c85eb4d0f7cb9f92b4cffb6a964d
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-08-08 20:28:54 +0530

    Add Create Account Controller

--- src/com/PlayTechPvtLtd/LiveChatApplication/controller/CreateNewUserAccountController.java
+++ src/com/PlayTechPvtLtd/LiveChatApplication/controller/CreateNewUserAccountController.java
@@ -1,16 +1,28 @@
 package com.PlayTechPvtLtd.LiveChatApplication.controller;
 
+import animatefx.animation.FadeIn;
+import com.PlayTechPvtLtd.LiveChatApplication.model.User;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
 public class CreateNewUserAccountController {
+    public static String username, password, gender;
+    public static ArrayList<User> loggedInUser = new ArrayList<>();
+    public static ArrayList<User> users = new ArrayList<User>();
     public TextField txtUserName;
     public PasswordField txtPassword;
     public TextField txtEmail;
@@ -34,16 +46,88 @@ public class CreateNewUserAccountController {
     public AnchorPane loginPane;
     public AnchorPane signUpPane;
 
-
     public void CreateAccountOnAction(ActionEvent actionEvent) {
+        if (!txtUserName.getText().equalsIgnoreCase("")
+                && !txtPassword.getText().equalsIgnoreCase("")
+                && !txtEmail.getText().equalsIgnoreCase("")
+                && !txtPhoneNumber.getText().equalsIgnoreCase("")
+                && (male.isSelected() || female.isSelected())) {
+            if (checkUser(txtUserName.getText())) {
+                if (checkEmail(txtEmail.getText())) {
+                    User newUser = new User();
+                    newUser.name = txtUserName.getText();
+                    newUser.password = txtPassword.getText();
+                    newUser.email = txtEmail.getText();
+                    newUser.phoneNo = txtPhoneNumber.getText();
+                    if (male.isSelected()) {
+                        newUser.gender = "Male";
+                    } else {
+                        newUser.gender = "Female";
+                    }
+                    users.add(newUser);
+                    goBack.setOpacity(1);
+                    success.setOpacity(1);
+                    makeDefault();
+                    if (controlRegLabel.getOpacity() == 1) {
+                        controlRegLabel.setOpacity(0);
+                    }
+                    if (nameExists.getOpacity() == 1) {
+                        nameExists.setOpacity(0);
+                    }
+                } else {
+                    checkEmail.setOpacity(1);
+                    setOpacity(nameExists, goBack, controlRegLabel, success);
+                }
+            } else {
+                nameExists.setOpacity(1);
+                setOpacity(success, goBack, controlRegLabel, checkEmail);
+            }
+        } else {
+            controlRegLabel.setOpacity(1);
+            setOpacity(success, goBack, nameExists, checkEmail);
+        }
     }
 
-    public void signUpInOnAction(ActionEvent actionEvent) {
+    private void setOpacity(Label a, Label b, Label c, Label d) {
+        if (a.getOpacity() == 1 || b.getOpacity() == 1 || c.getOpacity() == 1 || d.getOpacity() == 1) {
+            a.setOpacity(0);
+            b.setOpacity(0);
+            c.setOpacity(0);
+            d.setOpacity(0);
+        }
     }
 
-    public void backButtonOnAction(MouseEvent mouseEvent) {
+    private void setOpacity(Label controlRegLabel, Label checkEmail, Label nameExists) {
+        controlRegLabel.setOpacity(0);
+        checkEmail.setOpacity(0);
+        nameExists.setOpacity(0);
     }
 
-    public void loginOnAction(ActionEvent actionEvent) {
+    private void makeDefault() {
+        txtUserName.setText("");
+        txtPassword.setText("");
+        txtEmail.setText("");
+        txtPhoneNumber.setText("");
+        male.setSelected(true);
+        setOpacity(controlRegLabel, checkEmail, nameExists);
     }
+
+    private boolean checkUser(String username) {
+        for (User user : users) {
+            if (user.name.equalsIgnoreCase(username)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkEmail(String email) {
+        for (User user : users) {
+            if (user.email.equalsIgnoreCase(email)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
