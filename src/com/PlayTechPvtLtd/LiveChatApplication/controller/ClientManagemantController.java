858422f9cb5635cfd437c34cf21fd5dd4a4d0125
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-08-08 08:10:19 +0530

    Add client Manement Controller Class

---
+++ src/com/PlayTechPvtLtd/LiveChatApplication/controller/ClientManagemantController.java
@@ -0,0 +1,52 @@
+package com.PlayTechPvtLtd.LiveChatApplication.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+public class ClientManagemantController {
+
+    public Label clientName;
+    public ImageView imgMenu;
+    public AnchorPane profilePane;
+    public ImageView imgPhotoMain;
+    public Label lblUserName;
+    public Label lblEmail;
+    public Label lblPhone;
+    public Label lblGender;
+    public TextField fileChoosePath;
+    public AnchorPane chatPane;
+    public TextField txtMassage;
+    public TextArea txtArea;
+    public ImageView imgSend;
+    public ImageView imgPhoto;
+    public Circle showProPic;
+
+    public void imgMenuOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void imgCamaraOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void chooseImageButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    public void saveImageOnAction(ActionEvent actionEvent) {
+    }
+
+    public void sendMessageByKey(KeyEvent keyEvent) {
+    }
+
+    public void btnSendOnAction(MouseEvent mouseEvent) {
+    }
+}
